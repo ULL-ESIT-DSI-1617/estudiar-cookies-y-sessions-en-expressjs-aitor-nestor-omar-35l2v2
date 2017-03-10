6ab916c1ee78f7d20bd01b98d302377e742a4eb9
Aitor Bernal <alu0100830064@ull.edu.es>
2017-03-10 21:17:10 +0000

    Subiendo gulp

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -2,4 +2,10 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 
 
-// TODO 
+gulp.task('cookie', shell.task([
+    'node cookies-express.js'
+]));
+
+gulp.task('session', shell.task([
+    'node session-express.js'
+]));
